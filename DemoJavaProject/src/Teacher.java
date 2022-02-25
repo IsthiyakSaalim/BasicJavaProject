534d9ef8d116b4b34070bfa46d703b2330a4e7d7
IsthiyakSaalim <95282599+IsthiyakSaalim@users.noreply.github.com>
2022-02-25 19:17:36 +0530

    Created a new Teacher class

---
+++ DemoJavaProject/src/Teacher.java
@@ -0,0 +1,17 @@
+public class Teacher {
+
+  private int id;
+  private String name;
+  private String department;
+  
+  public Teacher() {
+  
+  }
+  
+  public Teacher(int id, String name, String department) {
+    this.id = id;
+    this.name = name;
+    this.department = department;
+  }
+  
+}
